9dfc86d629b1e41b76a01da1912f2382b92654d4
khd1879 <khd1879@gmail.com>
2026-09-13 18:24:16 +0900

    supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dsfrxhjltlxusllenfsu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Hp_Y8exNxlhi4bP45US1cA_TTI9zVAW";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
